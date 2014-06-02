e2c818109e1319e965c81f8d6645b9ad008ce134
Mikola Lysenko <mikolalysenko@gmail.com>
2014-06-02 17:51:15 -0500

    reduced test sizes and fixed up dependency on incremental-convex-hull

--- test/fuzz-2d.js
+++ test/fuzz-2d.js
@@ -5,12 +5,10 @@ var dt = require("../triangulate")
 var isDelaunay = require("./util/is-delaunay")
 
 tape("fuzz-2d", function(t) {
-  for(var i=0; i<10; ++i) {
-    var points = new Array(40)
-    for(var j=0; j<40; ++j) {
-      points[j] = [ Math.random(), Math.random() ]
-    }
-    isDelaunay(t, dt(points), points)
+  var points = new Array(40)
+  for(var j=0; j<40; ++j) {
+    points[j] = [ Math.random(), Math.random() ]
   }
+  isDelaunay(t, dt(points), points)
   t.end()
 })
